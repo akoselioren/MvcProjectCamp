c22a4cd3d505b7243b1c2580d0b0dce810945c72
Abdullah Köseliören <abdullahoren1@gmail.com>
2023-01-15 20:17:03 +0300

    Update
    Kullanıcı vitrin alanı oluşturuldu.
    Son açılan başlıklar listelendi.
    bütün başlıklar ana vitrine yansıtıldı.

---
+++ MvcProjectCamp/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjectCamp.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        HeadingManeger hm = new HeadingManeger(new EfHeadingDal());
+        ContentManager cm=new ContentManager(new EfContentDal());
+        public ActionResult Headings()
+        {
+            var headinglist=hm.GetList();
+            return View(headinglist);
+        }
+        public PartialViewResult Index()
+        {
+            var contentlist=cm.GetList();
+            return PartialView(contentlist);
+        }
+    }
+}
